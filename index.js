6e50c6796b20f15446ac034a55c52e3a87532db6
HawaryCon <ahmed.hwary38@gmail.com>
2023-03-24 16:47:16 +0400

    edit 3la package .json

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const { config } = require('dotenv');
 const routers = require('./routes/routes'); //
 config();
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(routers);
 
 const PORT = process.env.PORT || 7000;
 
@@ -15,4 +17,4 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
     .then(() => app.listen(PORT, () => console.log(`server is running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-app.use(routers);
+
