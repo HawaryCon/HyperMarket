bd1039ffa9462bc3b34fa1e2c181a824a64d9a82
HawaryCon <ahmed.hwary38@gmail.com>
2023-03-24 16:12:37 +0400

    updated vercel handler

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const PORT = process.env.PORT || 7000;
 app.use(routers);
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() =>console.log("Connected"))
-.catch((error) => console.log(error.message));
+    .then(() => console.log("Connected"))
+    .catch((error) => console.log(error.message));
 
-export default app;
 
+
+module.exports = { app }
+ 
---
+++ server.js
@@ -0,0 +1,2 @@
+const app =require("./index")
+app.app.listen(process.env.PORT || 7000)
