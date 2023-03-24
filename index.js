88fd52d16370b554450b62024445a8d53564b8cf
HawaryCon <ahmed.hwary38@gmail.com>
2023-03-24 16:01:44 +0400

    updated vercel config

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.use(cors());
 
 const PORT = process.env.PORT || 7000;
 app.use(routers);
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`server is running on port : ${PORT}`)))
-    .catch((error) => console.log(error.message));
 
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() =>console.log("Connected"))
+.catch((error) => console.log(error.message));
+
+export default app;
 
